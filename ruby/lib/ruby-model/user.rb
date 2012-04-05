1ba41d001eae0b91c27f33d417a72288a872ccf7
Brian Ploetz <bploetz@savewave.com>
2012-04-04 22:21:14 -0400

    Removing unnecessary fields from user

--- ruby/lib/ruby-model/user.rb
+++ ruby/lib/ruby-model/user.rb
@@ -4,6 +4,4 @@ class User
   key :rdbms_id, Integer
   key :username, String
   validates_uniqueness_of :username
-  many :posts, :class_name => 'Post'
-  many :comments, :class_name => 'Comment'
 end
